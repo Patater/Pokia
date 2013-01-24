3a31114f9b0247e2937b7492464a6f91939297b2
Jaeden Amero <jaeden@patater.com>
2013-01-24 00:00:32 -0600

    Add the NyanCat song

--- init.js
+++ init.js
@@ -22,3 +22,9 @@ var composerAxelF = '4g2 8.#a2 16g2 16- 16g2 8c3 8g2 8f2 4g2 8.d3 16g2 16-' +
 // https://en.wikipedia.org/wiki/Nokia_tune
 var rtttlNokiaTune =
     'Nokia:o=5,d=8,b=240:E6,D6,4F#,4G#,C#6,B,4D,4E,B,A,4C#,4E,2A.';
+
+var composerNyanCat =
+    '8#f3 8#g3 16#c3 8#d3 16#c3 16d3 16#c3 8b2 8b2 8#c3 8d3 16d3 16#c3 16b2' +
+    '16#c3 16#d3 16#f3 16#g3 16#d3 16#f3 16#c3 16#d3 16b2 16#c3 16b2 8#d3' +
+    '8#f3 16#g3 16#d3 16#f3 16#c3 16#d3 16b2 16#c3 16#d3 16d3 16#c3 16b2' +
+    '16#c3 8d3 16b2 16#c3 16#d3 16#f3 16#c3 16d3 16#c3 16b2 8#c3 8b2';
